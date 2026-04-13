9ddbbaf46b5b8627faac6f3ad4f91eaac92a5d38
Nikoloz <nikoloz.mamyan@lnstrade.fr>
2026-04-13 16:55:08 +0200

    upload image

---
+++ src/Model/ContactMessage.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class ContactMessage
+{
+    #[Assert\NotBlank(message: 'Votre nom est requis.')]
+    #[Assert\Length(max: 80)]
+    private ?string $name = null;
+
+    #[Assert\NotBlank(message: 'Votre email est requis.')]
+    #[Assert\Email(message: 'Email invalide.')]
+    #[Assert\Length(max: 120)]
+    private ?string $email = null;
+
+    #[Assert\Length(max: 30)]
+    private ?string $phone = null;
+
+    #[Assert\NotBlank(message: 'Sujet requis.')]
+    #[Assert\Length(max: 120)]
+    private ?string $subject = null;
+
+    #[Assert\NotBlank(message: 'Votre message est requis.')]
+    #[Assert\Length(min: 10, minMessage: 'Votre message doit faire au moins {{ limit }} caracteres.')]
+    #[Assert\Length(max: 2000)]
+    private ?string $message = null;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
